f1ecdae70ce8558dc649b42371175fac0b92a9fd
Charles Sarichith <charsari337@ed.amdsb.ca>
2021-02-11 11:46:30 -0500

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿
+namespace CashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxLan = new System.Windows.Forms.TextBox();
+            this.textBoxRope = new System.Windows.Forms.TextBox();
+            this.textBoxBomb = new System.Windows.Forms.TextBox();
+            this.totalsLabel = new System.Windows.Forms.Label();
+            this.CalcButt = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxTend = new System.Windows.Forms.TextBox();
+            this.calcChangeButt = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.receiptButt = new System.Windows.Forms.Button();
+            this.receiptLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Sienna;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(-1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Morshu\'s Shop";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Linen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lanterns";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Linen;
+            this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(185, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Rope";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Linen;
+            this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 105);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Bombs";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxLan
+            // 
+            this.textBoxLan.Location = new System.Drawing.Point(147, 54);
+            this.textBoxLan.Name = "textBoxLan";
+            this.textBoxLan.Size = new System.Drawing.Size(40, 20);
+            this.textBoxLan.TabIndex = 4;
+            // 
+            // textBoxRope
+            // 
+            this.textBoxRope.Location = new System.Drawing.Point(147, 80);
+            this.textBoxRope.Name = "textBoxRope";
+            this.textBoxRope.Size = new System.Drawing.Size(40, 20);
+            this.textBoxRope.TabIndex = 5;
+            // 
+            // textBoxBomb
+            // 
+            this.textBoxBomb.Location = new System.Drawing.Point(147, 106);
+            this.textBoxBomb.Name = "textBoxBomb";
+            this.textBoxBomb.Size = new System.Drawing.Size(40, 20);
+            this.textBoxBomb.TabIndex = 6;
+            // 
+            // totalsLabel
+            // 
+            this.totalsLabel.BackColor = System.Drawing.Color.Linen;
+            this.totalsLabel.Font = new System.Drawing.Font("Microsoft Tai Le", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalsLabel.Location = new System.Drawing.Point(12, 177);
+            this.totalsLabel.Name = "totalsLabel";
+            this.totalsLabel.Size = new System.Drawing.Size(185, 116);
+            this.totalsLabel.TabIndex = 7;
+            // 
+            // CalcButt
+            // 
+            this.CalcButt.BackColor = System.Drawing.Color.Sienna;
+            this.CalcButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CalcButt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalcButt.Location = new System.Drawing.Point(29, 145);
+            this.CalcButt.Name = "CalcButt";
+            this.CalcButt.Size = new System.Drawing.Size(146, 22);
+            this.CalcButt.TabIndex = 8;
+            this.CalcButt.Text = "Calculate Totals";
+            this.CalcButt.UseVisualStyleBackColor = false;
+            this.CalcButt.Click += new System.EventHandler(this.CalcButt_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Linen;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(13, 303);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Tendered:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBoxTend
+            // 
+            this.textBoxTend.Location = new System.Drawing.Point(100, 309);
+            this.textBoxTend.Name = "textBoxTend";
+            this.textBoxTend.Size = new System.Drawing.Size(87, 20);
+            this.textBoxTend.TabIndex = 11;
+            // 
+            // calcChangeButt
+            // 
+            this.calcChangeButt.BackColor = System.Drawing.Color.Sienna;
+            this.calcChangeButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.calcChangeButt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcChangeButt.Location = new System.Drawing.Point(29, 356);
+            this.calcChangeButt.Name = "calcChangeButt";
+            this.calcChangeButt.Size = new System.Drawing.Size(146, 25);
+            this.calcChangeButt.TabIndex = 12;
+            this.calcChangeButt.Text = "Calculate Change";
+            this.calcChangeButt.UseVisualStyleBackColor = false;
+            this.calcChangeButt.Click += new System.EventHandler(this.calcChangeButt_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.BackColor = System.Drawing.Color.Linen;
+            this.changeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(12, 393);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(184, 38);
+            this.changeLabel.TabIndex = 13;
+            this.changeLabel.Text = "Change:";
+            this.changeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // receiptButt
+            // 
+            this.receiptButt.BackColor = System.Drawing.Color.Sienna;
+            this.receiptButt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.receiptButt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptButt.Location = new System.Drawing.Point(29, 444);
+            this.receiptButt.Name = "receiptButt";
+            this.receiptButt.Size = new System.Drawing.Size(146, 25);
+            this.receiptButt.TabIndex = 14;
+            this.receiptButt.Text = "Print Receipt";
+            this.receiptButt.UseVisualStyleBackColor = false;
+            this.receiptButt.Click += new System.EventHandler(this.receiptButt_Click);
+            // 
+            // receiptLabel
+            // 
+            this.receiptLabel.BackColor = System.Drawing.Color.Linen;
+            this.receiptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.receiptLabel.Location = new System.Drawing.Point(228, 49);
+            this.receiptLabel.Name = "receiptLabel";
+            this.receiptLabel.Size = new System.Drawing.Size(444, 357);
+            this.receiptLabel.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(684, 481);
+            this.Controls.Add(this.receiptLabel);
+            this.Controls.Add(this.receiptButt);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.calcChangeButt);
+            this.Controls.Add(this.textBoxTend);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.CalcButt);
+            this.Controls.Add(this.totalsLabel);
+            this.Controls.Add(this.textBoxBomb);
+            this.Controls.Add(this.textBoxRope);
+            this.Controls.Add(this.textBoxLan);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxLan;
+        private System.Windows.Forms.TextBox textBoxRope;
+        private System.Windows.Forms.TextBox textBoxBomb;
+        private System.Windows.Forms.Label totalsLabel;
+        private System.Windows.Forms.Button CalcButt;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxTend;
+        private System.Windows.Forms.Button calcChangeButt;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Button receiptButt;
+        private System.Windows.Forms.Label receiptLabel;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CashRegister
+{
+    
+
+    public partial class Form1 : Form
+    {
+        double lanternsPrice = 15.75;
+        double ropePrice = 7.37;
+        double bombsPrice = 23.06;
+
+        double lanterns;
+        double rope;
+        double bombs;
+
+        double lanternsTotal;
+        double ropeTotal;
+        double bombsTotal;
+
+        double subTotal;
+        double totalCost;
+        double taxRate = 0.13;
+        double taxAmount;
+        double costwithTax;
+
+        double tenderedAmount;
+        double changeAmount;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBoxBomb.Text = "0";
+            textBoxRope.Text = "0";
+            textBoxLan.Text = "0";
+
+        }
+
+        private void CalcButt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                lanterns = Convert.ToInt16(textBoxLan.Text);
+                rope = Convert.ToInt16(textBoxRope.Text);
+                bombs = Convert.ToInt16(textBoxBomb.Text);
+
+                subTotal = (lanterns * lanternsPrice) + (rope * ropePrice) + (bombs * bombsPrice);
+                taxAmount = taxRate * subTotal;
+                totalCost = subTotal + taxAmount;
+
+                totalsLabel.Text = $"\nSub Total..............       {subTotal.ToString(".00")}";
+                totalsLabel.Text += $"\n\nTax...........................       {taxAmount.ToString(".00")}";
+                totalsLabel.Text += $"\n\nTotal.......................       {totalCost.ToString(".00")}";
+
+            }
+            catch
+            {
+                totalsLabel.Text = "Please insert desired amount of each item as a number.";
+            }
+
+        }
+
+        private void calcChangeButt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                tenderedAmount = Convert.ToInt16(textBoxTend.Text);
+                changeAmount = tenderedAmount - totalCost;
+
+                changeLabel.Text = $"\nChange: {changeAmount.ToString(".00")}";
+
+            }
+            catch
+            {
+                changeLabel.Text = "Numerial amount only.";
+                textBoxTend.Focus();
+            }
+        }
+
+        private void receiptButt_Click(object sender, EventArgs e)
+        {
+            lanternsTotal = lanternsPrice * lanterns;
+            ropeTotal = ropePrice * rope;
+            bombsTotal = bombsPrice * bombs;
+
+            receiptLabel.Text = "                                        Morshu's Shop";
+            Refresh();
+            Thread.Sleep(100);
+
+            receiptLabel.Text += $"\n\nCustomer Number 247";
+            receiptLabel.Text += $"\nFebruary 11, 2021";
+            receiptLabel.Text += $"\n\nLanterns:                    {lanterns}     x     @      {lanternsTotal.ToString("$.00")}";
+            receiptLabel.Text += $"\nRope:                          {rope}     x     @      {ropeTotal.ToString("$.00")}";
+            receiptLabel.Text += $"\nBombs:                       {bombs}     x     @      {bombsTotal.ToString("$.00")}";
+            receiptLabel.Text += $"\n\nSub Total:                 {subTotal.ToString("$.00")}";
+            receiptLabel.Text += $"\nTax:                            {taxAmount.ToString("$.00")}";
+            receiptLabel.Text += $"\nTotal:                         {totalCost.ToString("$.00")}";
+
+            receiptLabel.Text += $"\n\nTendered:                  {tenderedAmount.ToString("$.00")}";
+            receiptLabel.Text += $"\nChange:                     {changeAmount.ToString("$.00")}";
+
+            receiptLabel.Text += $"\n\nCome back to Morshu with more Rubies to spend!";
+        }
+
+    
+    }
+}
